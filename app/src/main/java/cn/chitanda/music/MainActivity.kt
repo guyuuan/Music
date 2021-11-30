bc1687d47b7d1686b244ff81abf60c63fdeebb98
chunjinchen <chunjinchen@smartmiglobal.com>
2021-11-30 10:42:28 +0800

    delete:

--- app/src/main/java/cn/chitanda/music/MainActivity.kt
+++ app/src/main/java/cn/chitanda/music/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        LinearInterpolator()
         setContent {
             MusicTheme {
                 Router()
