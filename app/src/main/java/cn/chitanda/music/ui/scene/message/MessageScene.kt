8c6ac486b9d8089add33d7a9e043d934615e95da
chunjinchen <chunjinchen1998@gmail.com>
2021-12-21 10:37:15 +0800

    modify: 将主题色预览合并至主题页面

--- app/src/main/java/cn/chitanda/music/ui/scene/message/MessageScene.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/message/MessageScene.kt
@@ -1,30 +1,10 @@
 package cn.chitanda.music.ui.scene.message
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import cn.chitanda.music.ui.LocalNavController
-import com.google.accompanist.insets.LocalWindowInsets
-import com.google.accompanist.insets.rememberInsetsPaddingValues
 import com.google.accompanist.pager.ExperimentalPagerApi
-import java.util.*
 
 /**
  *@author: Chen
@@ -38,69 +18,5 @@ private const val TAG = "MessageScene"
 @ExperimentalPagerApi
 @Composable
 fun MessageScene(navController: NavController = LocalNavController.current) {
-    val colors = MaterialTheme.colorScheme.toList()
-    LazyColumn(
-        modifier = Modifier.fillMaxWidth(), contentPadding = rememberInsetsPaddingValues(
-            insets = LocalWindowInsets.current.statusBars
-        )
-    ) {
-        items(colors) { item ->
-            ColorItem(color = item.first, name = item.second)
-        }
-    }
 }
 
-@Composable
-private fun ColorItem(color: Color, name: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(30.dp), verticalAlignment = Alignment.CenterVertically
-    ) {
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(color = color), contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "#${
-                    color.value.toString(16).subSequence(0, 7)
-                }".uppercase(Locale.getDefault()),
-                color = contentColorFor(backgroundColor = color)
-            )
-        }
-        Text(text = name, Modifier.weight(1f), textAlign = TextAlign.Center)
-    }
-}
-
-private fun ColorScheme.toList(): List<Pair<Color, String>> {
-    return listOf(
-        this.primary to "primary",
-        this.onPrimary to "onPrimary",
-        this.primaryContainer to "primaryContainer",
-        this.onPrimaryContainer to "onPrimaryContainer",
-        this.inversePrimary to "inversePrimary",
-        this.secondary to "secondary",
-        this.onSecondary to "onSecondary",
-        this.secondaryContainer to "secondaryContainer",
-        this.onSecondaryContainer to "onSecondaryContainer",
-        this.tertiary to "tertiary",
-        this.onTertiary to "onTertiary",
-        this.tertiaryContainer to "tertiaryContainer",
-        this.onTertiaryContainer to "onTertiaryContainer",
-        this.background to "background",
-        this.onBackground to "onBackground",
-        this.surface to "surface",
-        this.onSurface to "onSurface",
-        this.surfaceVariant to "surfaceVariant",
-        this.onSurfaceVariant to "onSurfaceVariant",
-        this.inverseSurface to "inverseSurface",
-        this.inverseOnSurface to "inverseOnSurface",
-        this.error to "error",
-        this.onError to "onError",
-        this.errorContainer to "errorContainer",
-        this.onErrorContainer to "onErrorContainer",
-        this.outline to "outline",
-    )
-}
--- app/src/main/java/cn/chitanda/music/ui/scene/other/ThemeScene.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/other/ThemeScene.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
@@ -16,7 +19,9 @@ import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -25,9 +30,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import cn.chitanda.music.ui.LocalThemeViewModel
@@ -36,6 +41,7 @@ import cn.chitanda.music.ui.scene.ThemeViewModel
 import cn.chitanda.music.ui.theme.MusicTheme
 import com.google.accompanist.insets.statusBarsHeight
 import kotlinx.coroutines.delay
+import java.util.*
 import kotlin.math.roundToInt
 
 /**
@@ -48,28 +54,31 @@ fun ThemeScene() {
     val themeViewModel = hiltViewModel<ThemeViewModel>()
     val primary by themeViewModel.monetColor
     Scaffold(Modifier.fillMaxSize(), backgroundColor = MaterialTheme.colorScheme.surfaceVariant) {
-        Column(Modifier.fillMaxWidth()) {
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Spacer(modifier = Modifier.statusBarsHeight(18.dp))
             ColorPicker(themeViewModel, MaterialTheme.colorScheme.primary)
-            Box(
-                modifier = Modifier.weight(1f)
-            ) {
-                ThemePreview(color = primary)
-            }
+            ThemePreview(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .weight(1f)
+                    .padding(vertical = 36.dp),
+                color = primary
+            )
+
         }
     }
 }
 
 @Composable
-fun ThemePreview(color: MonetColor?) {
+fun ThemePreview(modifier: Modifier = Modifier, color: MonetColor?) {
     MusicTheme(color) {
         Scaffold(
-            Modifier
-                .fillMaxSize()
-                .scale(0.5f),
+            modifier,
             backgroundColor = MaterialTheme.colorScheme.surface,
             topBar = {
-                TopAppBar(backgroundColor = MaterialTheme.colorScheme.inversePrimary) {}
+                TopAppBar(backgroundColor = MaterialTheme.colorScheme.inversePrimary) {
+                    Text(text = "ThemePreview")
+                }
             },
             floatingActionButton = {
                 FloatingActionButton(
@@ -84,6 +93,15 @@ fun ThemePreview(color: MonetColor?) {
                 }
             }
         ) {
+            val colors = MaterialTheme.colorScheme.toList()
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = it
+            ) {
+                items(colors) { item ->
+                    ColorItem(color = item.first, name = item.second)
+                }
+            }
         }
     }
 }
@@ -113,7 +131,7 @@ fun ColorPicker(themeViewModel: ThemeViewModel, themeColor: Color) {
                 .fillMaxWidth()
                 .height(50.dp)
         ) {
-            Text(text = "RED:$red", modifier = Modifier.weight(1f))
+            Text(text = "RED:$red", modifier = Modifier.weight(1.3f))
             Slider(
                 value = red.toFloat(),
                 onValueChange = { red = it.roundToInt() },
@@ -127,7 +145,7 @@ fun ColorPicker(themeViewModel: ThemeViewModel, themeColor: Color) {
                 .fillMaxWidth()
                 .height(50.dp)
         ) {
-            Text(text = "GREEN:$green", modifier = Modifier.weight(1f))
+            Text(text = "GREEN:$green", modifier = Modifier.weight(1.3f))
             Slider(
                 value = green.toFloat(),
                 onValueChange = { green = it.roundToInt() },
@@ -141,7 +159,7 @@ fun ColorPicker(themeViewModel: ThemeViewModel, themeColor: Color) {
                 .fillMaxWidth()
                 .height(50.dp)
         ) {
-            Text(text = "BLUE:$blue", modifier = Modifier.weight(1f))
+            Text(text = "BLUE:$blue", modifier = Modifier.weight(1.3f))
             Slider(
                 value = blue.toFloat(),
                 onValueChange = { blue = it.roundToInt() },
@@ -149,13 +167,6 @@ fun ColorPicker(themeViewModel: ThemeViewModel, themeColor: Color) {
                 modifier = Modifier.weight(4f)
             )
         }
-        Spacer(modifier = Modifier.height(24.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(40.dp)
-                .background(color = color)
-        )
         Spacer(modifier = Modifier.height(24.dp))
         TextButton(onClick = {
             globalThemeViewModel.setCustomThemeColor(color.toArgb())
@@ -172,4 +183,59 @@ fun ColorPicker(themeViewModel: ThemeViewModel, themeColor: Color) {
         delay(500)
         if (color != themeColor) themeViewModel.getMonetColor(color)
     }
+}
+
+@Composable
+private fun ColorItem(color: Color, name: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(30.dp), verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(color = color), contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "#${
+                    color.value.toString(16).subSequence(0, 7)
+                }".uppercase(Locale.getDefault()),
+                color = contentColorFor(backgroundColor = color)
+            )
+        }
+        Text(text = name, Modifier.weight(1f), textAlign = TextAlign.Center)
+    }
+}
+
+private fun ColorScheme.toList(): List<Pair<Color, String>> {
+    return listOf(
+        this.primary to "primary",
+        this.onPrimary to "onPrimary",
+        this.primaryContainer to "primaryContainer",
+        this.onPrimaryContainer to "onPrimaryContainer",
+        this.inversePrimary to "inversePrimary",
+        this.secondary to "secondary",
+        this.onSecondary to "onSecondary",
+        this.secondaryContainer to "secondaryContainer",
+        this.onSecondaryContainer to "onSecondaryContainer",
+        this.tertiary to "tertiary",
+        this.onTertiary to "onTertiary",
+        this.tertiaryContainer to "tertiaryContainer",
+        this.onTertiaryContainer to "onTertiaryContainer",
+        this.background to "background",
+        this.onBackground to "onBackground",
+        this.surface to "surface",
+        this.onSurface to "onSurface",
+        this.surfaceVariant to "surfaceVariant",
+        this.onSurfaceVariant to "onSurfaceVariant",
+        this.inverseSurface to "inverseSurface",
+        this.inverseOnSurface to "inverseOnSurface",
+        this.error to "error",
+        this.onError to "onError",
+        this.errorContainer to "errorContainer",
+        this.onErrorContainer to "onErrorContainer",
+        this.outline to "outline",
+    )
 }
