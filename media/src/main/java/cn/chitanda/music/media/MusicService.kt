850d4d6c1dab84b81933fd15ef82bf96487975e1
chunjinchen <chunjinchen1998@gmail.com>
2021-12-24 16:48:54 +0800

    new: 添加音乐播放相关的类

---
+++ media/src/main/java/cn/chitanda/music/media/MusicService.kt
@@ -0,0 +1,25 @@
+package cn.chitanda.music.media
+
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import androidx.media.MediaBrowserServiceCompat
+
+
+class MusicService : MediaBrowserServiceCompat() {
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ media/src/main/java/cn/chitanda/music/media/connect/MusicServiceConnection.kt
@@ -0,0 +1,13 @@
+package cn.chitanda.music.media.connect
+
+import android.content.ComponentName
+import android.content.Context
+
+/**
+ * @author: Chen
+ * @createTime: 2021/12/24 11:47 上午
+ * @description:
+ **/
+class MusicServiceConnection(context: Context, serviceComponent: ComponentName) {
+
+}
