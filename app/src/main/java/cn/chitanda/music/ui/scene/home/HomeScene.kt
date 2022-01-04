be57f96f8cf3e2349ef60cbbbb68fe6087e29962
chunjinchen <chunjinchen1998@gmail.com>
2022-01-04 18:24:30 +0800

    modify: 主页NavigationBar添加系统导航栏padding

--- app/src/main/java/cn/chitanda/music/ui/scene/home/HomeScene.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/home/HomeScene.kt
@@ -5,18 +5,22 @@ import androidx.annotation.StringRes
 import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -28,6 +32,8 @@ import cn.chitanda.music.ui.scene.find.FindScene
 import cn.chitanda.music.ui.scene.message.MessageScene
 import cn.chitanda.music.ui.scene.mine.MineScene
 import coil.annotation.ExperimentalCoilApi
+import com.google.accompanist.insets.navigationBarsHeight
+import com.google.accompanist.insets.navigationBarsPadding
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
@@ -102,27 +108,35 @@ private fun BottomBar(homeNavController: NavController) {
     var currentPage by remember {
         mutableStateOf<HomePageItem>(HomePageItem.Find)
     }
-    NavigationBar {
-        list.forEach { scene ->
-            NavigationBarItem(selected = currentPage == scene, onClick = {
-                currentPage = scene
-                homeNavController.navigate(scene.route) {
-                    popUpTo(homeNavController.graph.findStartDestination().id) {
-                        saveState = true
+    val containerColor = MaterialTheme.colorScheme.inverseOnSurface
+    Column {
+        NavigationBar(modifier = Modifier, containerColor = containerColor) {
+            list.forEach { scene ->
+                NavigationBarItem(selected = currentPage == scene, onClick = {
+                    currentPage = scene
+                    homeNavController.navigate(scene.route) {
+                        popUpTo(homeNavController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
                     }
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            }, icon = {
-                Icon(
-                    painter = painterResource(id = scene.icon),
-                    contentDescription = null
-                )
-            }, label = {
-                Text(text = stringResource(id = scene.label))
-            }, alwaysShowLabel = true)
+                }, icon = {
+                    Icon(
+                        painter = painterResource(id = scene.icon),
+                        contentDescription = null
+                    )
+                }, label = {
+                    Text(text = stringResource(id = scene.label))
+                }, alwaysShowLabel = true)
+            }
         }
-
+        Spacer(
+            modifier = Modifier
+                .background(color = containerColor)
+                .navigationBarsHeight()
+                .fillMaxWidth()
+        )
     }
 
 }
