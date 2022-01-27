0fbc55b14e9ceba6daf7103ccbe9ffc12a8f14f7
chunjinchen <chunjinchen1998@gmail.com>
2022-01-27 18:42:34 +0800

    new: 添加输入框焦点的切换功能,以及防止输入法遮挡界面

--- app/src/main/java/cn/chitanda/music/ui/scene/login/LoginScene.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/login/LoginScene.kt
@@ -2,31 +2,22 @@ package cn.chitanda.music.ui.scene.login
 
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
@@ -39,12 +30,14 @@ import cn.chitanda.music.http.DataState
 import cn.chitanda.music.ui.LocalNavController
 import cn.chitanda.music.ui.LocalUserViewModel
 import cn.chitanda.music.ui.Scene
+import com.google.accompanist.insets.imePadding
 
 /**
  *@author: Chen
  *@createTime: 2021/8/31 13:35
  *@description:
  **/
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LoginScene(
     viewModel: LoginViewModel = hiltViewModel(),
@@ -62,30 +55,50 @@ fun LoginScene(
     var password by remember {
         mutableStateOf("")
     }
+    val focusManager = LocalFocusManager.current
     Box(
         modifier = Modifier
+            .pointerInput(Unit) {
+                detectTapGestures {
+                    focusManager.clearFocus()
+                }
+            }
             .fillMaxSize()
             .background(color = MaterialTheme.colorScheme.primary),
         contentAlignment = Alignment.Center
     ) {
-        Surface(color = MaterialTheme.colorScheme.onPrimary, shape = RoundedCornerShape(16.dp)) {
+        Surface(
+            modifier = Modifier.imePadding(),
+            color = MaterialTheme.colorScheme.onPrimary,
+            shape = RoundedCornerShape(16.dp)
+        ) {
             Column(
-                modifier = Modifier.padding(
-                    start = 36.dp,
-                    top = 54.dp,
-                    end = 36.dp,
-                    bottom = 16.dp
-                ),
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier
+                    .padding(
+                        start = 36.dp,
+                        top = 54.dp,
+                        end = 36.dp,
+                        bottom = 16.dp
+                    )
+                    .fillMaxWidth(0.7f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                OutlinedTextField(value = accountName, onValueChange = {
-                    accountName = it
-                }, label = {
-                    Text(text = stringResource(R.string.text_enter_phone_number))
-                }, singleLine = true,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
+                OutlinedTextField(
+                    value = accountName,
+                    onValueChange = {
+                        accountName = it
+                    },
+                    label = {
+                        Text(text = stringResource(R.string.text_enter_phone_number))
+                    },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Phone,
+                        imeAction = ImeAction.Next
+                    ),
+                    modifier = Modifier.fillMaxWidth()
                 )
-                Spacer(modifier = Modifier.size(16.dp))
                 OutlinedTextField(
                     value = password, onValueChange = {
                         password = it
@@ -94,9 +107,16 @@ fun LoginScene(
                         Text(text = stringResource(R.string.text_enter_password))
                     },
                     visualTransformation = PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ), keyboardActions = KeyboardActions {
+                        focusManager.clearFocus()
+                        if (accountName.isNotEmpty() && password.isNotEmpty()) {
+                            viewModel.login(username = accountName, password = password)
+                        }
+                    }, modifier = Modifier.fillMaxWidth()
                 )
-                Spacer(modifier = Modifier.size(16.dp))
                 Button(shape = RoundedCornerShape(30.dp), onClick = {
                     if (accountName.isNotEmpty() && password.isNotEmpty()) {
                         viewModel.login(username = accountName, password = password)
